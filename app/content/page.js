d64d61dc10ed3b325ef203ad898af6e1c94cb47b
VelvetWhispers81 <mummabull81@gmail.com>
2026-06-23 13:52:16 -0500

    Update page.js

--- app/content/page.js
+++ app/content/page.js
@@ -1,1 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Velvet Whispers</h1>
+      <p>Soft spoken content. Early access. Exclusive whispers.</p>
 
+      <a href="/content">View Content</a>
+      <br />
+      <a href="/subscribe">Subscribe</a>
+    </main>
+  );
+}
